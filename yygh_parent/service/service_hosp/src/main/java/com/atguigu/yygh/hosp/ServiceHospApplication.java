3f76705c8e377957e4525eb64570b92373cb7b3a
@xxhhsw <2391019497@qq.com>
2021-06-08 19:59:38 +0800

    BUG修改

--- yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -3,10 +3,12 @@ package com.atguigu.yygh.hosp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.atguigu")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class,args);
